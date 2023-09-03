0fa4e91db142d3af45e07d27bc8b4558b9b8651f
Prashant Sing <psks0194@gmail.com>
2023-09-03 14:24:59 +0530

    coaching website

---
+++ public/script.js
@@ -0,0 +1,13 @@
+let navLinks = document.getElementById("navLinks");
+function showMenu() {
+  navLinks.style.top = "0";
+}
+function hideMenu() {
+  navLinks.style.top = "-800px";
+}
+
+function sendMessage() {
+  alert(`We will be in touch with you shortly. For an early connection please try phone number and email provided in contact details.
+         
+           Thanks in advance for your patience.`);
+}
